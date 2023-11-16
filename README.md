3a2f2c656c9a13b0ac72afa6f4318e16bdf4e23a
alisha-pradhan <alisha.subhamayee@gmail.com>
2023-11-16 20:23:43 +0530

    frontend part 1, basic pages created

---
+++ README.md
@@ -0,0 +1,12 @@
+We will be creatying a web application named, Babbal. 
+It will be a real time video streaming web application with live chat feature.
+
+This is the about page of the app. 
+All the key features, important updates all will be listed out here.
+
+
+Contributors, please remember, whenever you make any major change in the program file,
+make sure to mention and give a commit message. 
+Inline comments are greatly appreciated.
+
+This is a README.md file. Such files often include the project/app description. 
---
+++ frontend/room-select/room-select.js
@@ -0,0 +1,17 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const createRoom = document.getElementById('createRoom');
+//     const joinRoom = document.getElementById('joinRoom');
+//     const roomLinkInput = document.querySelector('.room-link');
+
+//     createRoom.addEventListener('click', function() {
+//         joinRoom.classList.remove('active');
+//         createRoom.classList.toggle('active');
+//         roomLinkInput.style.display = 'none';
+//     });
+
+//     joinRoom.addEventListener('click', function() {
+//         createRoom.classList.remove('active');
+//         joinRoom.classList.toggle('active');
+//         roomLinkInput.style.display = 'block';
+//     });
+// });
